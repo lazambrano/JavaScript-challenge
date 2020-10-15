5a90675dadec712d909ba774bcc1dda87e0de572
lazambrano <66233697+lazambrano@users.noreply.github.com>
2020-10-14 23:35:10 -0500

    Updated files to repo and committed changes

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,43 @@
-// from data.js
-var tableData = data;
+//table
+let varData = data;
 
-// YOUR CODE HERE!
+//have d3 select the table body
+let tbody = d3.select('tbody');
+
+varData.forEach(addRow);
+
+function addRow(item){
+    let row = tbody.append ('tr');
+    let result = Object.values(item);
+    result.forEach(data=>{
+        row.append('td').text(data)
+    })
+}
+
+//select button
+let button = d3.select("#filter-btn");
+
+//create the event handlers
+button.on("click", runEnter);
+
+//complete the event handler function for the form
+function runEnter()  {
+
+//keep the page from refreshing time after time when the enter key is hit
+d3.event.preventDefault();
+tbody.html('')
+
+//select the input element and get the row HTML 
+let inputElement = d3.select("#datetime");
+
+//get the value property of the input element
+let inputValue = inputElement.property('value');
+
+//console log and input value 2
+console.log(varData);
+
+//form input to filter the data 
+let filteredData = varData.filter(sighting => sighting.datetime === inputValue );
+filteredData.forEach(addRow)
+console.log(filteredData);
+};
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # JavaScript-challenge
+Created JavaScript code that created a table dynamically based upon the dataset provided. It allows users to filter the table data for specific values with JavaScript, HTML, and CSS, and D3.js within the web pages. 
+
+Created table and selected the table body with a select button. Created event handlers and completed
+the event handler function for the form to keep the page from refreshing time after time when the enter key is hit. 
+Selected the input element to get the row HTML and get the value property of the input element to console log and input value 2 to form input to filter the data.   
